4a220fd8d065535fc0030b63df9c9dc1092f8b71
Allen Phillip Pottinger <allenpottinger@gmail.com>
2020-04-13 17:44:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,11 +1,6 @@
+require_relative '.lib/concerns/memorable'
+
 class Student
-  
-  def hello 
-    puts "Hey there! I'm so excited to learn stuff."
-  end 
-  
-  def raise_hand
-    puts "Pick me!"
-  end 
-  
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
 end 
