ce531ab5e7a20599ec1def93cde8a7c7b940da6c
Allen Phillip Pottinger <allenpottinger@gmail.com>
2020-04-13 17:43:22 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -15,11 +15,15 @@ module Memorable
   
   
   module InstanceMethods
-   
-  end 
+    
+    def hello 
+      puts "Hey there! I'm so excited to learn stuff."
+    end 
   
+    def raise_hand
+      puts "Pick me!"
+    end 
   
-  
-  
+  end
   
 end
